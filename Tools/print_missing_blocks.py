fe96cda137b0755e0665baa5f7c799737c48b336
Robert Reichel <robertreich1212@gmail.com>
2020-07-20 14:45:51 +0200

    fix block_textures.json, tweak print_missing_blocks.py a bit

--- Tools/print_missing_blocks.py
+++ Tools/print_missing_blocks.py
@@ -5,16 +5,21 @@ import math
 texturesPath = '../Block Textures/'
 
 count = 0
+count1 = 0
 
 with open('block_textures.json') as json_file:
   textures = json.load(json_file)
   for filename,copies in textures.items():
+    if os.path.isfile(texturesPath + filename):
+      count1 += 1
+      print(filename + ' | is present')
     if not os.path.isfile(texturesPath + filename):
       count += 1
-      print(filename)
+      print(filename + ' | is missing')
 
   total = len(textures)
   print('----------------------------------------')
-  print('  Total missing: ' + str(count))
-  print('  Complete:      ' + str(math.floor((total - count) * 100 / total)) + '%')
+  print('  Total missing textures: ' + str(count))
+  print('  Total present textures: ' + str(count1))
+  print('  Percentage complete:    ' + str(math.floor((total - count) * 100 / total)) + '%')
   print('----------------------------------------')
