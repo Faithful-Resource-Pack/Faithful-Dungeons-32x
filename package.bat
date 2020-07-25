ff05a102291c8f85f9228a30a1a725bedefcc61e
Julien <Juknum@users.noreply.github.com>
2020-07-25 19:48:52 +0200

    replaced REM by echo command

--- package.bat
+++ package.bat
@@ -4,19 +4,19 @@ SET "ddp=%ddp:~0,-1%"
 SET /p editorPath= < Tools\user_settings\editor_directory.txt
 SET /p packageOutput= < Tools\user_settings\package_output.txt
 
-REM Building Resource Pack...
+echo Building Resource Pack...
 cd Tools
 python py\copy_block_textures.py
 cd..
 
-REM Importing assets...
+echo Importing assets...
 @echo off
 python Tools\py\generate_asset_import_settings.py
 "%editorPath%\UE4Editor-Cmd.exe" "%ddp%\UE4Project\Dungeons.uproject" -run=ImportAssets -nosourcecontrol "-importsettings=%ddp%\Tools\tmp_import_settings.json"
 del /s Tools\tmp_import_settings.json  >nul 2>&1
 @echo on
 
-@REM Cooking assets...
+echo Cooking assets...
 del /S Dungeons\*.uasset
 del /S Dungeons\*.ubulk
 del /S Dungeons\*.uexp
@@ -25,7 +25,7 @@ del /S Dungeons\*.ufont
 "%editorPath%\UE4Editor-Cmd.exe" "%ddp%\UE4Project\Dungeons.uproject" -run=cook -targetplatform=WindowsNoEditor
 robocopy /job:Tools\configs\copy_cooked_assets
 
-REM	Packing...
+echo Packing...
 python Tools\py\u4pak.py pack "%packageOutput%" Dungeons -p
 
 echo Packing is done. You can close this window now.
