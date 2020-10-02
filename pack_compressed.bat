1b7bf7e95d992884873ae01da21b54b1bb2d851e
Robert Reichel <robertreich1212@gmail.com>
2020-10-02 15:53:54 +0200

    add titles to package.bat, add texture that is only one color, add various uasset files

--- pack_compressed.bat
+++ pack_compressed.bat
@@ -1,7 +1,8 @@
 @echo off
+title [1/5] Applying settings...
 echo ------------------------------------------------------------
 echo(
-echo Applying settings...
+echo [1/5] Applying settings...
 echo(
 echo ------------------------------------------------------------
 SET "ddp=%~dp0"
@@ -9,27 +10,30 @@ SET "ddp=%ddp:~0,-1%"
 SET /p editorPath= < Tools\user_settings\editor_directory.txt
 SET /p packageOutput= < Tools\user_settings\package_output.txt
 
+title [2/5] Building resource pack...
 echo ------------------------------------------------------------
 echo(
-echo Building resource pack...
+echo [2/5] Building resource pack...
 echo(
 echo ------------------------------------------------------------
 cd Tools
 python py\copy_block_textures.py
 cd..
 
+title [3/5] Importing assets...
 echo ------------------------------------------------------------
 echo(
-echo Importing assets...
+echo [3/5] Importing assets...
 echo(
 echo ------------------------------------------------------------
 python Tools\py\generate_asset_import_settings.py
 "%editorPath%\UE4Editor-Cmd.exe" "%ddp%\UE4Project\Dungeons.uproject" -run=ImportAssets -nosourcecontrol "-importsettings=%ddp%\Tools\tmp_import_settings.json"
 del /s Tools\tmp_import_settings.json  >nul 2>&1
 
+title [4/5] Cooking assets...
 echo ------------------------------------------------------------
 echo(
-echo Cooking assets...
+echo [4/5]Cooking assets...
 echo(
 echo ------------------------------------------------------------
 del /S Dungeons\*.uasset
@@ -40,9 +44,10 @@ del /S Dungeons\*.ufont
 "%editorPath%\UE4Editor-Cmd.exe" "%ddp%\UE4Project\Dungeons.uproject" -run=cook -targetplatform=WindowsNoEditor
 robocopy /job:Tools\configs\copy_cooked_assets
 
+title [5/5] Packing...
 echo ------------------------------------------------------------
 echo(
-echo Packing...
+echo [5/5] Packing...
 echo(
 echo ------------------------------------------------------------
 python Tools\py\u4pak.py pack "compressed_pack.pak" Dungeons -z -p
